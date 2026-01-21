fcda94a734225b82c95360f02433fb542e995d00
fredbec <f-becker@posteo.de>
2026-01-21 16:34:24 +0100

    move filter calls to separate files

---
+++ scripts/filter_spf/filter_spf_months.R
@@ -0,0 +1,42 @@
+library(here)
+library(data.table)
+
+source(here("scripts", "filter_spf", "filter_spf_functions.R"))
+source(here("scripts", "filter_spf", "filter_spf_loaddata.R"))
+#chain operator for data.table
+DT <- `[`
+
+for(shft in -1:1){
+  res_spf_filter <- vector(mode = "list", length = nrow(combs))
+  res_spf_additionalinfo <- vector(mode = "list", length = nrow(combs))
+
+  for(i in 1:nrow(combs)){
+
+    cissue <- combs[i,]
+
+    cqu <- cissue$quarter
+    cyr <- cissue$year
+
+    print(cissue)
+    res <- filter_dat(current_quarter = cqu,
+                      current_year = cyr,
+                      SPF_data = spfdat,
+                      real_time_data = rtd,
+                      rtd_issue = "latest_vintage",
+                      rtd_shift = shft)
+
+    res_spf_filter[[i]] <- res$spf_filter_dat
+    res_spf_additionalinfo[[i]] <- data.table(origin_quarter = cqu,
+                                              origin_year = cyr,
+                                              cy_logLik = res$ll["cy"],
+                                              cyandny_logLik = res$ll["cyandny"],
+                                              cy_rw_sd = res$params$cy["rw_sd"],
+                                              cyandny_rw_sd = res$params$cyandny["rw_sd"])
+  }
+  res_spf_filter <- rbindlist(res_spf_filter)
+  res_spf_additionalinfo <- rbindlist(res_spf_additionalinfo)
+
+  data.table::fwrite(res_spf_filter, here("data", paste0("filter_spf_data_medianfc", "_month", shft+2, ".csv")))
+  data.table::fwrite(res_spf_additionalinfo, here("data", paste0("filter_spf_data_medianfc_supplementary", "_month", shft+2, ".csv")))
+}
+
