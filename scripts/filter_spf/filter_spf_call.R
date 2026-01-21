d6e3a2236e6c0e731ba459f55ef3d4d49f5a813e
fredbec <f-becker@posteo.de>
2026-01-21 21:42:28 +0100

    master filter call

---
+++ scripts/filter_spf/filter_spf_call.R
@@ -0,0 +1,13 @@
+library(here)
+library(data.table)
+
+source(here("scripts", "filter_spf", "filter_spf_functions.R"))
+source(here("scripts", "filter_spf", "filter_spf_loaddata.R"))
+
+
+source(here("scripts", "filter_spf", "filter_spf_base.R"))
+source(here("scripts", "filter_spf", "filter_spf_individualforecasters.R"))
+source(here("scripts", "filter_spf", "filter_spf_months.R"))
+source(here("scripts", "filter_spf", "filter_spf_withus.R"))
+source(here("scripts", "filter_spf", "filter_spf_gammaest.R"))
+source(here("scripts", "filter_spf", "filter_spf_approxerror.R"))
