2721e17e6cbbe2bbccd8c30e418579e6a196a969
fredbec <f-becker@posteo.de>
2026-01-27 11:19:55 +0100

    script to run filter using settings

---
+++ scripts/02_run_kalman.R
@@ -0,0 +1,29 @@
+library(here)
+library(data.table)
+DT <- `[`
+
+source(here("scripts", "filter_spf", "filter_spf_functions.R"))
+
+
+
+for(spec_id in c("consensus_median", "consensus_mean", "individual")){
+
+  spec <- read_spec(spec_id)
+
+  runs <- read.csv(
+    file.path("output/filter_spf", spec_id, "runs.csv"),
+    stringsAsFactors = FALSE
+  )
+
+
+for (i in seq_len(nrow(runs))) {
+  run <- as.list(runs[i, ])
+  print(run)
+  settings <- merge_reformat_settings(spec, run)
+
+  message("Running ", spec_id, " / ", run$run_id, " / ", run$name)
+
+  results <- run_filter_from_settings(settings)
+  write_outputs(results, settings)
+}
+}
